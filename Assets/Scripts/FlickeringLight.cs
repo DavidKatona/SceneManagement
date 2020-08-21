c9994f1b33f7a3626d7d9e5f34476079d6056253
dave94punk <david.katona@hotmail.com>
2020-08-21 15:17:59 +0200

    Initial commit

---
+++ Assets/Scripts/FlickeringLight.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FlickeringLight : MonoBehaviour
+{
+    private Light _lightSource;
+    [SerializeField] private float _lightSourceDurationOn;
+    [SerializeField] private float _lightSourceDurationOff;
+    [SerializeField] private bool _startLit;
+    private bool _isFlickering;
+
+    private void Awake()
+    {
+        _lightSource = GetComponent<Light>();
+
+        float startingIntensity = (_startLit) ? _lightSource.intensity = 1 : _lightSource.intensity = 0;
+        _lightSource.intensity = startingIntensity;
+    }
+
+    void Update()
+    {
+        if (!_isFlickering)
+        {
+            StartCoroutine(Flicker(_lightSourceDurationOn, _lightSourceDurationOff));
+        }
+    }
+
+    private IEnumerator Flicker(float durationOn, float durationOff)
+    {
+        _isFlickering = true;
+        yield return new WaitForSeconds(durationOn);
+
+        _lightSource.intensity = 0;
+
+        yield return new WaitForSeconds(durationOff);
+        _lightSource.intensity = 1;
+        _isFlickering = false;
+    }
+}
---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class Loader
+{
+    // Just a dummy class on which implements MonoBehaviour.
+    private class LoadingMonoBehaviour : MonoBehaviour { }
+
+    public enum Scene
+    {
+        SceneA,
+        SceneB,
+        LoadingScene
+    }
+
+    private static Action onLoaderCallback;
+    private static AsyncOperation _loadingAsyncOperation;
+
+    public static void Load(Scene scene)
+    {
+        // Before we load the loading scene, we assign an anonymous function to our static onLoaderCallback action, which will be called by the LoaderCallback method.
+        onLoaderCallback = () =>
+        {
+            // We create an empty GameObject which will hold a component that implements MonoBehaviour so we can call StartCoroutine on that object.
+            GameObject loadingGameObject = new GameObject("Loading Game Object");
+            loadingGameObject.AddComponent<LoadingMonoBehaviour>().StartCoroutine(LoadSceneAsync(scene));
+        };
+
+        SceneManager.LoadScene(Scene.LoadingScene.ToString());
+
+    }
+
+    /// <summary>
+    /// A coroutine that asynchronously loads a target scene.
+    /// </summary>
+    /// <param name="scene">Scene to load.</param>
+    /// <returns></returns>
+    private static IEnumerator LoadSceneAsync(Scene scene)
+    {
+        yield return null;
+        _loadingAsyncOperation = SceneManager.LoadSceneAsync(scene.ToString());
+
+        while (!_loadingAsyncOperation.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    public static float GetLoadingProgress()
+    {
+        if (_loadingAsyncOperation != null)
+        {
+            return _loadingAsyncOperation.progress;
+        }
+        else
+        {
+            return 1f;
+        }
+    }
+
+    /// <summary>
+    /// Invokes the static onLoaderCallback action of the static Loader class if it is not null.
+    /// </summary>
+    public static void LoaderCallback()
+    {
+        onLoaderCallback?.Invoke();
+    }
+}
